1c7d553fb6829ab1878d23df197bfff97e77e003
Phat Future <hungphat0511@gmail.com>
2019-11-25 16:15:56 +0700

    add CRUDControler.php

--- db/reset_db.sh
+++ db/reset_db.sh
@@ -2,12 +2,12 @@
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 APP_HOME="$SCRIPT_HOME/../.."
 
-c=hungphat_falcon_intern_postgres  # c aka container
+c=hungphat_laravel_restful_api  # c aka container
 
 docker cp "$APP_HOME/SQL/CreateDatabase.sql" $c:/
 docker cp "$APP_HOME/SQL/CreateCustomer.sql" $c:/
 docker cp "$APP_HOME/SQL/InsertList.sql"     $c:/
 
-docker exec $c bash -c "psql -Umysql                 -f /CreateDatabase.sql"
-docker exec $c bash -c "psql -Umysql -d customerdata -f /CreateCustomer.sql"
-docker exec $c bash -c "psql -Umysql -d customerdata -f /InsertList.sql"
+docker exec $c bash -c "mysql -Uroot                 -f /CreateDatabase.sql"
+docker exec $c bash -c "mysql -Uroot -d customerdata -f /CreateCustomer.sql"
+docker exec $c bash -c "mysql -Uroot -d customerdata -f /InsertList.sql"
